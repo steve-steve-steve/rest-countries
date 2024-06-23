fe4c2aed27b6ff73223de8aa02bd71c0fe348aa4
Steve Green <steve@greengob.com>
2024-06-23 23:30:51 +0100

    fetch the countries and utilise ag-grid to display the fields of interest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
+import {AgGridReact} from 'ag-grid-react'; // React Data Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+
 
 function App() {
+  const gridRef = useRef();
+
+  const [countries, setCountries] = useState([])
+  const [colDefs] = useState([
+    {field: "name", flex: 1, filter: true, sort: "asc"},
+    {field: "flag", flex: 0.5},
+    {field: "population", flex: 1},
+    {field: "languages", flex: 3, filter: true},
+    {field: "currencies", flex: 2, filter: true},
+  ]);
+
+  useEffect(() => {
+
+    const fetchCountries = async () => {
+      try {
+        const {data} = await axios.get('https://restcountries.com/v3.1/all')
+        let countries = data.map(e => ({
+          name: e.name.common,
+          flag: e.flag,
+          population: new Intl.NumberFormat().format(Number(e.population)),
+          languages: e.languages ? Object.values(e.languages).join(", ") : "N/A",
+          currencies: e.currencies ? buildCurrencylist(Object.values(e.currencies)) : "N/A",
+        }))
+        setCountries(countries);
+      } catch (error) {
+        console.error("Error getting countries:", error);
+      }
+    }
+    fetchCountries()
+  }, [countries]);
+
+  const buildCurrencylist = (currencies) => currencies.map(c => c.name).join(", ")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        className="ag-theme-quartz"
+        style={{height: 500}}
+      >
+        <AgGridReact
+          ref={gridRef}
+          rowData={countries}
+          columnDefs={colDefs}
+          rowSelection={"single"}
+        />
+      </div>
     </div>
   );
 }
